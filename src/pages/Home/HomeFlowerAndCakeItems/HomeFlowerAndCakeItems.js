dd5fbefc203e2ea6424bdbcd188513b94558bc2b
afianasrin1 <afia.nasrin3e@gmail.com>
2022-11-09 13:56:54 +0600

    blog section compleat

--- src/pages/Home/HomeFlowerAndCakeItems/HomeFlowerAndCakeItems.js
+++ src/pages/Home/HomeFlowerAndCakeItems/HomeFlowerAndCakeItems.js
@@ -5,7 +5,7 @@ const HomeFlowerAndCakeItems = () => {
   return (
     <div className="mb-16 mt-28">
       <div className="flex  justify-between border-b border-gray-400 pb-5">
-        <h1 className="text-4xl font-semibold">RECENT ADDED</h1>
+        <h1 className="text-4xl font-semibold">RECENT ADDED REVIEW</h1>
         <Link
           to="/recipes"
           className="flex items-center bg-red-500 py-2 px-3 rounded-md hover:bg-red-600 text-white"
--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -28,11 +28,13 @@ const Blog = () => {
               title="Visit the East"
               className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
             >
-              Visit the East
+              difference between sql and nosql?
             </a>
             <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
+              SQL is the programming language used to interface with relational
+              databases. (Relational databases model data as records in rows and
+              tables with logical links between them). NoSQL is a class of DBMs
+              that are non-relational and generally do not use SQL.
             </p>
             <a
               href="/"
@@ -67,11 +69,14 @@ const Blog = () => {
               title="Simple is better"
               className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
             >
-              Simple is better
+              What is JWT, and how does it work?
             </a>
             <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
+              What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open
+              standard (RFC 7519) for securely transmitting information between
+              parties as JSON object. It is compact, readable and digitally
+              signed using a private key/ or a public key pair by the Identity
+              Provider(IdP).Apr 25, 2022
             </p>
             <a
               href="/"
@@ -106,11 +111,55 @@ const Blog = () => {
               title="Film It!"
               className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
             >
-              Film It!
+              What is the difference between javascript and NodeJS?
             </a>
             <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
+              Javascript is a programming language that is used for writing
+              scripts on the website. Javascript can only be run in the
+              browsers.Javascript can run in any browser engine as like JS core
+              in safari and Spidermonkey in Firefox.We can run Javascript
+              outside the browser with the help of NodeJS.It is mostly used on
+              the server-side.V8 is the Javascript engine inside of node.js that
+              parses and runs Javascript.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <a
+                href="/"
+                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+                aria-label="Category"
+                title="traveling"
+              >
+                traveling
+              </a>
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+            <a
+              href="/"
+              aria-label="Category"
+              title="Film It!"
+              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              How does NodeJS handle multiple requests at the same time?
+            </a>
+            <p className="mb-2 text-gray-700">
+              NodeJS receives multiple client requests and places them into
+              EventQueue. NodeJS is built with the concept of event-driven
+              architecture. NodeJS has its own EventLoop which is an infinite
+              loop that receives requests and processes them. EventLoop is the
+              listener for the EventQueue. If NodeJS can process the request
+              without I/O blocking then the event loop would itself process the
+              request and sends the response back to the client by itself. But,
+              it is possible to process multiple requests parallelly using the
+              NodeJS cluster module or worker_threads module.
             </p>
             <a
               href="/"
